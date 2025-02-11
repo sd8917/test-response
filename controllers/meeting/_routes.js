65f86fa74afab2e96cbeed5d58fa13e81bf13110
Sudhanshu kumar <sudhanshuraj8917@gmail.com>
2025-02-11 16:18:49 +0530

    add meeting code

--- controllers/meeting/_routes.js
+++ controllers/meeting/_routes.js
@@ -1,5 +1,11 @@
 const express = require('express');
-
 const router = express.Router();
+const meetingController = require('./meeting');
 
-module.exports = router
+router.post('/', meetingController.add);
+router.get('/', meetingController.index);
+router.get('/:id', meetingController.view);
+router.delete('/:id', meetingController.deleteData);
+router.delete('/', meetingController.deleteMany);
+
+module.exports = router;
--- controllers/meeting/meeting.js
+++ controllers/meeting/meeting.js
@@ -1,24 +1,56 @@
-const MeetingHistory = require('../../model/schema/meeting')
+const MeetingHistory = require('../../model/schema/meeting');
 const mongoose = require('mongoose');
 
 const add = async (req, res) => {
-   
-}
+    try {
+        const meeting = new MeetingHistory(req.body);
+        await meeting.save();
+        res.status(201).send(meeting);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+};
 
 const index = async (req, res) => {
-    
-}
+    try {
+        const meetings = await MeetingHistory.find({});
+        res.status(200).send(meetings);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+};
 
 const view = async (req, res) => {
-    
-}
+    try {
+        const meeting = await MeetingHistory.findById(req.params.id);
+        if (!meeting) {
+            return res.status(404).send();
+        }
+        res.status(200).send(meeting);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+};
 
 const deleteData = async (req, res) => {
-  
-}
+    try {
+        const meeting = await MeetingHistory.findByIdAndDelete(req.params.id);
+        if (!meeting) {
+            return res.status(404).send();
+        }
+        res.status(200).send(meeting);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+};
 
 const deleteMany = async (req, res) => {
-    
-}
+    try {
+        const result = await MeetingHistory.deleteMany({ _id: { $in: req.body.ids } });
+        res.status(200).send(result);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+};
 
-module.exports = { add, index, view, deleteData, deleteMany }
+module.exports = { add, index, view, deleteData, deleteMany };
